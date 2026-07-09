038116da57c385b63a5b62ba0cf53a3d8a1e91a3
ch0412 <pce0412010@gmail.com>
2026-07-09 19:50:35 +0900

    chore: add Flyway and init DB schema (#3)

--- src/test/java/com/gold/safefam/SafefamApplicationTests.java
+++ src/test/java/com/gold/safefam/SafefamApplicationTests.java
@@ -3,7 +3,7 @@ package com.gold.safefam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class SafefamApplicationTests {
 
 	@Test
